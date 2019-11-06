d78bd3052923dee9a32a46b39ebf89eb61da1442
cat23x <baumgart.guenter02@gmail.com>
2019-11-06 14:22:20 +0100

    3Pos switch

---
+++ shredderControl/3pos.cpp
@@ -0,0 +1,60 @@
+#include "3pos.h"
+#include "enums.h"
+
+Pos3::Pos3(int upPin, int downPin)
+{
+  this->upPin = upPin;
+  this->downPin = downPin;
+}
+
+int Pos3::setup()
+{
+
+  this->debouncerUp = Bounce();
+  this->debouncerUp.attach(this->upPin, INPUT_PULLUP);
+  this->debouncerUp.interval(25);
+
+  this->debouncerDown = Bounce();
+  this->debouncerDown.attach(this->downPin, INPUT_PULLUP);
+  this->debouncerDown.interval(25);
+  return 0;
+}
+
+int Pos3::read()
+{
+
+  this->debouncerUp.update();
+  this->debouncerDown.update();
+
+  bool up = this->debouncerUp.read() == 0 ? true : false;
+  bool down = this->debouncerDown.read() == 0 ? true : false;
+
+  int newDirection = 0;
+
+  if (up)
+  {
+    newDirection = POS3_DIRECTION::UP;
+  }
+  if (down)
+  {
+    newDirection = POS3_DIRECTION::DOWN;
+  }
+  if (!up && !down)
+  {
+    newDirection = POS3_DIRECTION::MIDDLE;
+  }
+  return newDirection;
+}
+
+int Pos3::loop()
+{
+  int newDirection = this->read();
+
+  if (newDirection != this->switch_pos)
+  {
+    this->last_switch = this->switch_pos;
+  }
+  this->switch_pos = newDirection;
+
+  return this->switch_pos;
+}
