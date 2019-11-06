c51adda36cf99d2aa7a58af1f4e7bf23b4919685
cat23x <baumgart.guenter02@gmail.com>
2019-11-06 14:50:52 +0100

    3Pos switch activated

---
+++ shredderControl/Pos3.h
@@ -0,0 +1,26 @@
+#ifndef POS3_H
+#define POS3_H
+
+#include <Bounce2.h>
+
+class Pos3
+{
+public:
+   Pos3(int upPin, int downPin);
+
+   int setup();
+   int loop();
+   int read();
+
+protected:
+   int upPin;
+   int downPin;
+
+   int last_switch = -1; // Track last switch position
+   int switch_pos = -1;  // Current switch position
+
+   Bounce debouncerUp;
+   Bounce debouncerDown;
+};
+
+#endif
